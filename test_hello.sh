959abd45ab19b34d8c0fc5c1f714afe4dfd676e0
ashokgit2405 <39010785+ashokgit2405@users.noreply.github.com>
2021-06-12 11:29:06 +0530

    Update test_hello.sh

--- test_hello.sh
+++ test_hello.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-[[ `./hello.sh johnny` = "hello johnny!" ]] && (echo "test passed!"; exit 0) || (echo "test failed :-("; exit 1)
+[[ `./hello.sh johnny2` = "hello johnny2!" ]] && (echo "test passed!"; exit 0) || (echo "test failed :-("; exit 1)
 
 
